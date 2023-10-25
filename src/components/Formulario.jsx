c74a8e7c9e3c458fe79343bf04c1b06e93c22270
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-10-25 14:27:30 -0500

    Creando formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,8 @@
 import styled from "@emotion/styled"
+import useSelectMonedas from "../hooks/useSelectMonedas"
+import { monedas } from "../data/monedas"
+import { useEffect, useState } from "react"
+
 
 const InputSubmit = styled.input`
     background-color: #9497FF;
@@ -12,6 +16,7 @@ const InputSubmit = styled.input`
     border-radius: 5px;
     transition: background-color .3s ease;
     
+    
     &:hover {
         background-color: #7a7dfe;
         cursor: pointer;
@@ -19,13 +24,48 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
+
+    const [ criptos, setCriptos ] = useState([]);
+
+    const [ moneda, SelectModedas ] = useSelectMonedas('Elige la moneda', monedas)
+    const [ criptomoneda, SelectCriptomoneda ] = useSelectMonedas('Elige la criptomoneda', criptos)
+    
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
+
+            const respuesta = await fetch(url)
+            const resultado = await respuesta.json()
+            
+
+            const arrayCriptos = resultado.Data.map( cripto => {
+
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                return objeto
+            })
+
+            setCriptos(arrayCriptos);
+
+        }
+        consultarAPI();
+    }, [])
+
+    
+
   return (
     <form>
 
+        <SelectModedas />
+        <SelectCriptomoneda />
+       
         <InputSubmit 
             type="submit" 
-            value="cotizar" 
+            value="cotizar"
         />
+
     </form>
   )
 }
